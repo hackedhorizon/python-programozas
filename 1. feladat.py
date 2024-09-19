f5b8886d130f62a5a099489be6ad6b8c2e7a0699
cybernexus <raczdwork@proton.me>
2024-09-19 12:17:42 +0200

    Upload first task.

---
+++ 1. feladat.py
@@ -0,0 +1,42 @@
+# Mire való a print utasítás?
+# Kiíratunk vele valamit
+
+# Mi lehet ez a valami?
+# - szöveg
+# - szám
+# - változó
+# - matematikai műveletek
+
+# Hogyan írnánk ki a képernyőre azt a szöveget, hogy szia?
+print("szia")
+
+# Hogyan írnánk ki a képernyőre azt, hogy 5?
+print(5)
+
+# Írjuk ki a képernyőre a 3 és a 7 összeadásának eredményét:
+# Aritmetikai műveletek (+ - * / %)
+print(3 + 7)   # Összeadás
+print(5 - 2)   # Kivonás
+print(10 * 2)  # Szorzás
+print(10 / 2)  # Osztás
+print(10 % 2)  # Maradékos osztás
+print(10 ** 4) # Hatványozás
+
+# Egyszerre több aritmetikai műveletet is elvégezhetünk:
+print(5 - 2 + 2 * 10)
+
+# Változók
+# A változók a számítógép memóriájában (RAM)-ban kerülnek eltárolásra.
+# Egy változónak adhatunk nevet, később erre a névre hivatkozhatunk.
+# A változókban adatokat tárolhatunk, mint például:
+bármi           = (3 + 2)   # Egy összeadás eredményét
+nemtudom        = "alma"    # Egy szöveges típusú adatot
+logikaiValtozo  = True      # Egy logikai igaz értéket
+egészSzám       = 10        # Egy egész számot
+
+# Egy korábban már létrehozott változó tartalmát felülírhatjuk, úgy, hogy
+# a nevére hivatkozva (leírva) egy új értéket adunk neki:
+logikaiValtozo  = False
+
+# Változtassuk meg a "nemtudom" nevű változó tartalmát "alma"-ról "banán"-ra:
+nemtudom        = "banán"
---
+++ házifeladat.py
@@ -0,0 +1,73 @@
+'''
+    1. feladat: Kiíratás szöveggel
+    Írj egy programot, amely a következő szöveget írja ki a képernyőre:
+    Hello, Python!
+
+    Segítség: Használd a print utasítást, és írd a szöveget idézőjelek közé!
+'''
+
+'''
+    2. feladat: Kiíratás számmal
+    Írj egy programot, amely kiírja a képernyőre a következő számokat:
+    
+    5
+    10
+    Segítség: A print utasítással számokat is ki tudsz íratni!
+'''
+
+'''
+    3. feladat: Aritmetikai műveletek
+    Írj egy programot, amely elvégzi a következő műveleteket, és kiírja az eredményüket:
+    
+    7 + 3
+    10 - 4
+    6 * 5
+    9 / 3
+
+Segítség: Minden műveletet külön print utasítással írj ki!
+'''
+
+'''
+    4. feladat: Változók létrehozása
+    Hozz létre egy szam nevű változót, aminek az értéke legyen 10!
+    Hozz létre egy szoveg nevű változót, aminek az értéke legyen "python"!
+    Írasd ki ezeket a változókat a képernyőre!
+    Segítség: A változók létrehozásához a következő formát használhatod:
+    
+    valtozoNev = érték
+'''
+
+'''
+    5. feladat: Műveletek változókkal
+    Hozz létre két változót:
+    
+    x legyen 5
+    y legyen 8
+    Számold ki az x és az y összeadásának eredményét, majd írasd ki a képernyőre!
+    
+    Segítség: A változók értékeit felhasználhatod úgy is, mintha számok lennének. Próbáld ki:
+    
+    print(x + y)
+'''
+
+'''
+    6. feladat: Változók felülírása
+    Hozz létre egy gyumolcs nevű változót, aminek az értéke legyen "alma".
+    Írasd ki a változót a képernyőre.
+    Módosítsd a gyumolcs változó értékét "banán"-ra, és írasd ki újra!
+    Segítség: Egy változó értékét bármikor megváltoztathatod, csak írd felül az új értékkel.
+'''
+
+'''
+    7. feladat: Kis kihívás – Műveletek kombinálása
+    Hozz létre két változót:
+    
+    a legyen 10
+    b legyen 3
+    Számold ki a következő műveleteket, és írasd ki az eredményeket:
+    
+    a + b
+    a * b
+    a / b
+    Ne aggódj, ha valamelyik feladat nehéznek tűnik! Gyakorolj, és ha elakadsz, nézd meg újra a korábbi példákat.
+'''
