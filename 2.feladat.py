25709e96d24cb39cfc2f8297455ab098cf7a89c7
cybernexus <raczdwork@proton.me>
2024-09-27 16:27:47 +0200

    Upload homework.

---
+++ 2.feladat.py
@@ -0,0 +1,33 @@
+
+# Összegzés
+print("Ez egy sor \nEz egy másik sor")
+
+# print utasítás
+
+# változók (létrehozás, kiolvasás, értékmódosítás)
+
+# adattípusok (szöveg, szám, logikai)
+# aritmetikai műveletek
+
+# Hozzunk létre egy szöveges adattípusú változót, majd írjuk ki a tartalmát
+valtozo = "amit csak szeretnél"
+print(valtozo)
+# Hozzunk létre egy szám típusú változót, majd írjuk ki a tartalmát
+szám = "-1"
+print(szám)
+# Hozzunk létre egy logikai típusú változót, majd írjuk ki a tartalmát
+logikai = True
+print(logikai)
+# Adjunk össze 4db. számot, majd írjuk ki a tartalmát. Hányféleképpen oldhatjuk ezt meg?
+szam1 = 4
+szam2 = 4
+szam3 = 4
+szam4 = 4
+
+print(szam1 + szam2 + szam3 + szam4)
+
+
+# Írjuk ki print segítségével a nevünket 15 alkalommal egymás után
+nev = "Milán\n"
+szam5 = 15
+print(nev * szam5)
