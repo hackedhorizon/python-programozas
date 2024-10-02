e39ee7bf37b48fba3330743ffecd025c8d2da085
cybernexus <raczdwork@proton.me>
2024-10-02 17:05:13 +0200

    Upload homework.

---
+++ 3. óra/1. feladat.py
@@ -0,0 +1,30 @@
+
+
+
+
+
+
+valtozo1 = "6"
+valtozo2 = 6
+
+
+
+
+
+# Kérjünk be egy számot, szorozzuk meg 5-tel majd írjuk ki az eredményt
+szam = int(input("Kérem adjon meg egy számot: "))
+szam1 = 5
+print(szam*szam1)
+
+
+
+
+
+# Kérjünk be egy felhasználónevet és egy jelszót
+felhasznalonev = input("kérem adjon meg egy fehasználo nevet")
+jelszo = input("Kérem adjon meg egy jelszot")
+print("üdvozlom",felhasznalonev , "\nez a jelszava",jelszo)
+# Kérjünk be két számot, majd adjuk őket össze
+
+# Írjuk ki a változókat tájékoztató üzenettel!
+
