ecbedc0dbd15d9563f162c394058d6a7c8b0b8cf
cybernexus <raczdwork@proton.me>
2024-10-06 14:00:42 +0200

    Upload assignment.

---
+++ 4. óra/1. feladat.py
@@ -0,0 +1,25 @@
+
+# Próbáljunk meg egy szöveget kiíratni a képernyőre idézőjelek nélkül.
+# Milyen hibát kapunk?
+print ("szoveg")
+
+# Próbáljunk meg egy szöveget kiíratni a képernyőre zárójel(ek) nélkül.
+# Milyen hibát kapunk?
+print ("szoveg")
+
+# Írjunk ki a képernyőre egy nem létező változót.
+# Milyen hibát kapunk?
+
+#print (valtozo)
+# Hozzunk létre egy szöveges típusú változót
+# Majd próbáljuk meg átalakítani szám típusúvá kiíratásnál.
+# Milyen hibát kapunk?
+
+
+
+valtozo  = str(4)
+print (valtozo)
+
+vezetéknév = input ("Kérem adja meg a vezeték nevét!")
+keresztnév = input ("Kérem adja meg a kereszt nevét!")
+print ("Üdvözöllek",vezetéknév, keresztnév)
