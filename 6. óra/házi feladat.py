e6909c4f067a8dd50bedc3171dc092f33ee46ba4
cybernexus <raczdwork@proton.me>
2024-10-23 14:11:13 +0200

    Upload homework.

---
+++ 6. óra/házi feladat.py
@@ -0,0 +1,60 @@
+"""
+
+1. Videójáték
+    Feladat:
+    Egy gyermek csak akkor játszhat videójátékokkal, ha megcsinálta a házi feladatát és kitakarította a szobáját.
+    Írd meg a programot, amely eldönti, hogy játszhat-e a gyermek!
+    Változók:
+
+    - hazifeladatkesz (True vagy False értékű)
+    - kitakaritottame (True vagy False értékű)
+
+    Használj logikai and operátort a feltétel ellenőrzéséhez!
+
+2. Séta az esőben
+    Feladat: Egy baráti társaság akkor megy sétálni, ha nem esik az eső vagy van esernyőjük.
+    Írj egy programot, amely megállapítja, hogy elindulhatnak-e sétálni!
+    Változók:
+
+    - esikAzEso (True vagy False értékű)
+    - vanEsernyo (True vagy False értékű)
+    Használj logikai not és or operátorokat a feltétel megadásához!
+
+
+3. Kirándulás a hétvégén
+    Feladat: Egy diák akkor mehet el kirándulni a hétvégén, ha tanult a vizsgájára vagy a tanára megengedte neki.
+    Készíts egy programot, amely eldönti, hogy elmehet-e kirándulni!
+    Változók:
+
+    - készultemAVizsgara (True vagy False értékű)
+    - megengedteATanar (True vagy False értékű)
+
+    Használj logikai or operátort!
+
+4. Összehasonlítás két szám között
+    Feladat: Írj egy programot, amely összehasonlít két számot!
+    Kérd be a felhasználótól az első és a második számot, majd döntsd el a következőket:
+
+    - Az első szám kisebb, mint a második?
+    - Az első szám nagyobb, mint a második?
+    - Az első szám egyenlő a másodikkal?
+
+    Használj összehasonlító operátorokat: <, >, ==!
+
+5. Gyümölcs összehasonlítás
+    Feladat: Készíts egy programot, amely megvizsgálja, hogy egy banán nem ugyanaz, mint egy alma!
+    Változók:
+
+    - gyumolcs1 = "banán"
+    - gyumolcs2 = "alma"
+
+    Használj logikai != (nem egyenlő) operátort!
+
+6. Nagyobb vagy egyenlő vizsgálata
+    Feladat: Írj egy programot, amely megállapítja, hogy egy adott szám nagyobb, kisebb vagy egyenlő egy másik számmal!
+    Kérd be a felhasználótól mindkét számot, és használd a következő összehasonlító operátorokat:
+
+    >= nagyobb vagy egyenlő
+    <= kisebb vagy egyenlő
+
+"""
